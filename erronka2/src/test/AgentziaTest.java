3299aacc46e611b860d47b23ea43aff5eb797873
Alex Saez <alex.saezot@elorrieta-errekamari.com>
2025-02-07 08:16:53 +0100

    test gustiak

---
+++ erronka2/src/test/AgentziaTest.java
@@ -0,0 +1,80 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import modelo.Agentzia;
+
+public class AgentziaTest {
+
+    private Agentzia agentzia;
+
+    @Before
+    public void setUp() {
+        agentzia = new Agentzia(1, "L3", "www.www.com", "urdina", "admin", "A1", "admin");
+    }
+
+   
+    @Test
+    public void testGetSetAgentziaKodea() {
+    	 agentzia.setAgentziaKodea(2);
+         assertEquals(2, agentzia.getAgentziaKodea());
+    }
+
+
+    @Test
+    public void testGetSetLangileKopKod() {
+    	 agentzia.setLangileKopKod("L10");
+         assertEquals("L10", agentzia.getLangileKopKod());
+    }
+
+   
+
+    @Test
+    public void testGetSetLogoa() {
+    	agentzia.setLogoa("logo.png");
+        assertEquals("logo.png", agentzia.getLogoa());
+    }
+
+   
+
+    @Test
+    public void testGetSetMarkarenKolorea() {
+    	agentzia.setMarkarenKolorea("gorria");
+        assertEquals("gorria", agentzia.getMarkarenKolorea());
+    }
+
+   
+
+    @Test
+    public void testGetIzena() {
+    	  agentzia.setIzena("alexjav");
+          assertEquals("alexjav", agentzia.getIzena());
+    }
+
+ 
+
+    @Test
+    public void testGetSetAgentziaMKod() {
+    	 agentzia.setAgentziaMKod("A2");
+         assertEquals("A2", agentzia.getAgentziaMKod());
+    }
+
+    
+
+    @Test
+    public void testGetSetPasahitza() {
+    	agentzia.setPasahitza("alexjav");
+        assertEquals("alexjav", agentzia.getPasahitza());
+    }
+
+    
+
+    @Test
+    public void testToString() {
+        String expected = "Agentzia [agentziaKodea=1, langileKopKod=L3, logoa=www.www.com, markarenKolorea=urdina, izena=admin, agentziaMKod=A1, pasahitza=admin]";
+        assertEquals(expected, agentzia.toString());
+    }
+}
---
+++ erronka2/src/test/AirelineaTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import modelo.Airelinea;
+
+public class AirelineaTest {
+
+	private Airelinea airelinea;
+	@Before
+	public void setUp() throws Exception {
+		airelinea = new Airelinea("A1","Loiu","H1");
+	}
+
+	
+
+	@Test
+	public void testGetSetAirelineaKodea() {
+		airelinea.setAirelineaKodea("A2");
+		assertEquals("A2",airelinea.getAirelineaKodea());
+	}
+
+	
+	@Test
+	public void testGetSetAirelineIzena() {
+		airelinea.setAirelineIzena("Santander");;
+		assertEquals("Santander",airelinea.getAirelineIzena());
+	
+	}
+
+	
+
+	@Test
+	public void testGetSetHerrialdeKodea() {
+		airelinea.setHerrialdeKodea("H2");
+		assertEquals("H2",airelinea.getHerrialdeKodea());
+	
+	}
+
+	
+
+}
