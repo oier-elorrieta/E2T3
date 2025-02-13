7c21139909cfb1837f6daa5b925275bad59f4b81
Alexander González <in1dm3-d@elorrieta.int>
2025-02-13 11:55:16 +0100

    azken aldaketak

--- src/test/ZerbitzuTest.java
+++ src/test/ZerbitzuTest.java
@@ -12,24 +12,25 @@ import modelo.pojo.Zerbitzu;
 
 public class ZerbitzuTest {
 
-	 private Zerbitzu ostatuZerbitzu;
-	    private Zerbitzu hegaldiZerbitzu;
-	    private Zerbitzu besteBatzukZerbitzu;
-	    private Zerbitzu joanEtorriZerbitzu;
-	    private Zerbitzu zerbitzu;
-	    private Date ostatuSarreraEguna;
-	    private Date ostatuIrteraEguna;
-	    private Date hegaldiIrteraData;
-	    private Time hegaldiIrteeraOrdutegia;
-	    private Time hegaldiBidaiarenIraupena;
-	    private Date egun;
-	    private Time itzuleraOrdua;
-	    private Date etorriaEguna;
-	    private Time bueltakoIraupena;
+    private Zerbitzu ostatuZerbitzu;
+    private Zerbitzu hegaldiZerbitzu;
+    private Zerbitzu besteBatzukZerbitzu;
+    private Zerbitzu joanEtorriZerbitzu;
+    private Zerbitzu zerbitzu;
+    private Date ostatuSarreraEguna;
+    private Date ostatuIrteraEguna;
+    private Date hegaldiIrteraData;
+    private Time hegaldiIrteeraOrdutegia;
+    private Time hegaldiBidaiarenIraupena;
+    private Date egun;
+    private Time itzuleraOrdua;
+    private Date etorriaEguna;
+    private Time bueltakoIraupena;
 
-	@Before
-	public void setUp() {
-		ostatuSarreraEguna = Date.valueOf("2025-01-01");
+    // Test setup metodoa
+    @Before
+    public void setUp() {
+        ostatuSarreraEguna = Date.valueOf("2025-01-01");
         ostatuIrteraEguna = Date.valueOf("2025-01-10");
         hegaldiIrteraData = Date.valueOf("2025-01-01");
         hegaldiIrteeraOrdutegia = Time.valueOf("10:00:00");
@@ -39,6 +40,7 @@ public class ZerbitzuTest {
         etorriaEguna = Date.valueOf("2025-01-02");
         bueltakoIraupena = Time.valueOf("02:00:00");
 
+        // Zerbitzuen hasierapena
         ostatuZerbitzu = new Zerbitzu(1, "Ostatu Zerbitzua", 1, 100, ostatuSarreraEguna, ostatuIrteraEguna, "Bilbo", "L1");
         hegaldiZerbitzu = new Zerbitzu(2, "Hegaldi Zerbitzua", 2, "H1", hegaldiIrteraData, hegaldiIrteeraOrdutegia, hegaldiBidaiarenIraupena, 200, "BIO", "MAD", "A1");
         besteBatzukZerbitzu = new Zerbitzu(3, "Beste Zerbitzua", 3, egun, "Deskribapena", 50);
@@ -47,240 +49,196 @@ public class ZerbitzuTest {
                                           hegaldiBidaiarenIraupena, 150.0f, "JFK", "LAX", "A123", "HE124", 
                                           itzuleraOrdua, etorriaEguna, bueltakoIraupena, "LAX", "JFK", "A124");
         zerbitzu = new Zerbitzu();
-	}
+    }
+    
+    // Zerbitzu kodea testatzen duen metodoa
+    @Test
+    public void testGetSetZerbitzuKodea() {
+        ostatuZerbitzu.setZerbitzuKodea(10);
+        assertEquals(10, ostatuZerbitzu.getZerbitzuKodea());
+    }
 
-	
+    // Zerbitzu izena testatzen duen metodoa
+    @Test
+    public void testGetSetZerbitzuIzena() {
+        ostatuZerbitzu.setZerbitzuIzena("New Ostatu Zerbitzua");
+        assertEquals("New Ostatu Zerbitzua", ostatuZerbitzu.getZerbitzuIzena());
+    }
 
-	
-	
-	@Test
-	public void testGetSetZerbitzuKodea() {
-		ostatuZerbitzu.setZerbitzuKodea(10);
-		assertEquals(10, ostatuZerbitzu.getZerbitzuKodea());
-	}
+    // Bidai mota kodea testatzen duen metodoa
+    @Test
+    public void testGetSetBidaiMotaKodea() {
+        ostatuZerbitzu.setBidaiKodea(4);
+        assertEquals(4, ostatuZerbitzu.getBidaiKodea());
+    }
 
-	
+    // Ostatu prezioa testatzen duen metodoa
+    @Test
+    public void testGetSetOstatuPrezioa() {
+        ostatuZerbitzu.setOstatuPrezioa(150);
+        assertEquals(150, ostatuZerbitzu.getOstatuPrezioa(), 0.01);
+    }
 
-	@Test
-	public void testGetSetZerbitzuIzena() {
-		ostatuZerbitzu.setZerbitzuIzena("New Ostatu Zerbitzua");
-		assertEquals("New Ostatu Zerbitzua", ostatuZerbitzu.getZerbitzuIzena());
-	}
+    // Ostatu sarrera eguna testatzen duen metodoa
+    @Test
+    public void testGetSetOstatuSarreraEguna() {
+        Date newDate = Date.valueOf("2025-02-01");
+        ostatuZerbitzu.setOstatuSarreraEguna(newDate);
+        assertEquals(newDate, ostatuZerbitzu.getOstatuSarreraEguna());
+    }
 
+    // Ostatu irtera eguna testatzen duen metodoa
+    @Test
+    public void testGetSetOstatuIrteraEguna() {
+        Date newDate = Date.valueOf("2025-02-10");
+        ostatuZerbitzu.setOstatuIrteraEguna(newDate);
+        assertEquals(newDate, ostatuZerbitzu.getOstatuIrteraEguna());
+    }
 
-	@Test
-	public void testGetSetBidaiMotaKodea() {
-		ostatuZerbitzu.setBidaiKodea(4);
-		assertEquals(4, ostatuZerbitzu.getBidaiKodea());
-	}
+    // Hiria testatzen duen metodoa
+    @Test
+    public void testGetSetHiria() {
+        ostatuZerbitzu.setHiria("Donostia");
+        assertEquals("Donostia", ostatuZerbitzu.getHiria());
+    }
 
-	
+    // Logela mota kodea testatzen duen metodoa
+    @Test
+    public void testGetSetLogelaMKodea() {
+        ostatuZerbitzu.setLogelaMKodea("L2");
+        assertEquals("L2", ostatuZerbitzu.getLogelaMKodea());
+    }
 
-	@Test
-	public void testGetSetOstatuPrezioa() {
-		ostatuZerbitzu.setOstatuPrezioa(150);
-		assertEquals(150, ostatuZerbitzu.getOstatuPrezioa(), 0.01);
-	}
+    // Egun data testatzen duen metodoa
+    @Test
+    public void testGetSetEgun() {
+        Date newDate = Date.valueOf("2025-01-20");
+        besteBatzukZerbitzu.setEgun(newDate);
+        assertEquals(newDate, besteBatzukZerbitzu.getEgun());
+    }
 
-	
+    // Beste zerbitzu deskribapena testatzen duen metodoa
+    @Test
+    public void testGetSetBesteBatzukDeskribapena() {
+        besteBatzukZerbitzu.setBesteBatzukDeskribapena("Deskribapena2");
+        assertEquals("Deskribapena2", besteBatzukZerbitzu.getBesteBatzukDeskribapena());
+    }
 
-	@Test
-	public void testGetSetOstatuSarreraEguna() {
-		Date newDate = Date.valueOf("2025-02-01");
-		ostatuZerbitzu.setOstatuSarreraEguna(newDate);
-		assertEquals(newDate, ostatuZerbitzu.getOstatuSarreraEguna());
-	}
+    // Beste zerbitzu prezioa testatzen duen metodoa
+    @Test
+    public void testGetSetBesteBatzukPrezioa() {
+        besteBatzukZerbitzu.setBesteBatzukPrezioa(75);
+        assertEquals(75, besteBatzukZerbitzu.getBesteBatzukPrezioa(), 0.01);
+    }
 
-	
+    // Hegaldi kodea testatzen duen metodoa
+    @Test
+    public void testGetSetHegaldiKodea() {
+        hegaldiZerbitzu.setHegaldiKodea("H2");
+        assertEquals("H2", hegaldiZerbitzu.getHegaldiKodea());
+    }
 
-	@Test
-	public void testGetSetOstatuIrteraEguna() {
-		Date newDate = Date.valueOf("2025-02-10");
-		ostatuZerbitzu.setOstatuIrteraEguna(newDate);
-		assertEquals(newDate, ostatuZerbitzu.getOstatuIrteraEguna());
-	}
+    // Hegaldi irtera data testatzen duen metodoa
+    @Test
+    public void testGetSetHegaldiIrteraData() {
+        Date newDate = Date.valueOf("2025-02-01");
+        hegaldiZerbitzu.setHegaldiIrteraData(newDate);
+        assertEquals(newDate, hegaldiZerbitzu.getHegaldiIrteraData());
+    }
 
-	
+    // Hegaldi irteera ordua testatzen duen metodoa
+    @Test
+    public void testGetSetHegaldiIrteeraOrdutegia() {
+        Time newHegaldiIrteeraOrdutegia = Time.valueOf("22:00:00");
+        hegaldiZerbitzu.setHegaldiIrteeraOrdutegia(newHegaldiIrteeraOrdutegia);
+        assertEquals(newHegaldiIrteeraOrdutegia, hegaldiZerbitzu.getHegaldiIrteeraOrdutegia());
+    }
 
-	@Test
-	public void testGetSetHiria() {
-		ostatuZerbitzu.setHiria("Donostia");
-		assertEquals("Donostia", ostatuZerbitzu.getHiria());
-	}
+    // Hegaldi bidaiaren iraupena testatzen duen metodoa
+    @Test
+    public void testGetSetHegaldiBidaiarenIraupena() {
+        Time newHegaldiBidaiarenIraupena = Time.valueOf("03:00:00");
+        hegaldiZerbitzu.setHegaldiBidaiarenIraupena(newHegaldiBidaiarenIraupena);
+        assertEquals(newHegaldiBidaiarenIraupena, hegaldiZerbitzu.getHegaldiBidaiarenIraupena());
+    }
 
-	
+    // Hegaldi prezioa testatzen duen metodoa
+    @Test
+    public void testGetSetHegaldiPrezioa() {
+        hegaldiZerbitzu.setHegaldiPrezioa(22);
+        assertEquals(22, hegaldiZerbitzu.getHegaldiPrezioa(), 0.01);
+    }
 
-	
+    // Hegaldi jatorrizko aireportua testatzen duen metodoa
+    @Test
+    public void testGetSetHegaldiJatorrizkoAireportua() {
+        hegaldiZerbitzu.setHegaldiJatorrizkoAireportua("Malaga");
+        assertEquals("Malaga", hegaldiZerbitzu.getHegaldiJatorrizkoAireportua());
+    }
 
-	
+    // Hegaldi helmugako aireportua testatzen duen metodoa
+    @Test
+    public void testGetSetHegaldiHelmugakoAireportua() {
+        hegaldiZerbitzu.setHegaldiHelmugakoAireportua("Malaga");
+        assertEquals("Malaga", hegaldiZerbitzu.getHegaldiHelmugakoAireportua());
+    }
 
-	@Test
-	public void testGetSetLogelaMKodea() {
-		ostatuZerbitzu.setLogelaMKodea("L2");
-		assertEquals("L2", ostatuZerbitzu.getLogelaMKodea());
-	}
+    // Airelina kodea testatzen duen metodoa
+    @Test
+    public void testGetSetAirelinaKodea() {
+        hegaldiZerbitzu.setAirelinaKodea("A2");
+        assertEquals("A2", hegaldiZerbitzu.getAirelinaKodea());
+    }
 
-	
+    // Itzulera ordua testatzen duen metodoa
+    @Test
+    public void testGetSetItzuleraOrdua() {
+        Time newItzuleraOrdua = Time.valueOf("03:00:00");
+        hegaldiZerbitzu.setItzuleraOrdua(newItzuleraOrdua);
+        assertEquals(newItzuleraOrdua, hegaldiZerbitzu.getItzuleraOrdua());
+    }
 
-	@Test
-	public void testGetSetEgun() {
-		Date newDate = Date.valueOf("2025-01-20");
-		besteBatzukZerbitzu.setEgun(newDate);
-		assertEquals(newDate, besteBatzukZerbitzu.getEgun());
-	}
+    // Etorria eguna testatzen duen metodoa
+    @Test
+    public void testGetSetEtorriaEguna() {
+        Date newDate = Date.valueOf("2025-02-17");
+        hegaldiZerbitzu.setEtorriaEguna(newDate);
+        assertEquals(newDate, hegaldiZerbitzu.getEtorriaEguna());
+    }
 
-	
+    // Bueltako iraupena testatzen duen metodoa
+    @Test
+    public void testGetSetBueltakoIraupena() {
+        Time newBueltakoIraupena = Time.valueOf("03:00:00");
+        hegaldiZerbitzu.setBueltakoIraupena(newBueltakoIraupena);
+        assertEquals(newBueltakoIraupena, hegaldiZerbitzu.getBueltakoIraupena());
+    }
 
-	@Test
-	public void testGetSetBesteBatzukDeskribapena() {
-		besteBatzukZerbitzu.setBesteBatzukDeskribapena("Deskribapena2");
-		assertEquals("Deskribapena2", besteBatzukZerbitzu.getBesteBatzukDeskribapena());
-	}
+    // Joan jatorrizko aireportua testatzen duen metodoa
+    @Test
+    public void testGetSetJoanJatorrizkoAireportua() {
+        hegaldiZerbitzu.setJoanJatorrizkoAireportua("Santander");
+        assertEquals("Santander", hegaldiZerbitzu.getJoanJatorrizkoAireportua());
+    }
 
-	
+    // Joan helmugako aireportua testatzen duen metodoa
+    @Test
+    public void testGetSetJoanHelmugakoAireportua() {
+        hegaldiZerbitzu.setJoanHelmugakoAireportua("Santander");
+        assertEquals("Santander", hegaldiZerbitzu.getJoanHelmugakoAireportua());
+    }
 
-	@Test
-	public void testGetSetBesteBatzukPrezioa() {
-		besteBatzukZerbitzu.setBesteBatzukPrezioa(75);
-		assertEquals(75, besteBatzukZerbitzu.getBesteBatzukPrezioa(), 0.01);
-	}
+    // Bueltako airelinea kodea testatzen duen metodoa
+    @Test
+    public void testGetSetBueltakoAirelineaKodea() {
+        hegaldiZerbitzu.setBueltakoAirelineaKodea("B2");
+        assertEquals("B2", hegaldiZerbitzu.getBueltakoAirelineaKodea());
+    }
 
-	
-
-	@Test
-	public void testGetSetHegaldiKodea() {
-		hegaldiZerbitzu.setHegaldiKodea("H2");
-		assertEquals("H2", hegaldiZerbitzu.getHegaldiKodea());
-	}
-
-	
-
-	@Test
-	public void testGetSetHegaldiIrteraData() {
-		Date newDate = Date.valueOf("2025-02-01");
-		hegaldiZerbitzu.setHegaldiIrteraData(newDate);
-		assertEquals(newDate, hegaldiZerbitzu.getHegaldiIrteraData());
-	}
-
-	
-
-	@Test
-	public void testGetSetHegaldiIrteeraOrdutegia() {
-		Time newHegaldiIrteeraOrdutegia = Time.valueOf("22:00:00");
-		hegaldiZerbitzu.setHegaldiIrteeraOrdutegia(newHegaldiIrteeraOrdutegia);
-		assertEquals(newHegaldiIrteeraOrdutegia,hegaldiZerbitzu.getHegaldiIrteeraOrdutegia());
-	}
-
-	
-
-	@Test
-	public void testGetSetHegaldiBidaiarenIraupena() {
-		Time newHegaldiBidaiarenIraupena = Time.valueOf("03:00:00");
-		hegaldiZerbitzu.setHegaldiBidaiarenIraupena(newHegaldiBidaiarenIraupena);
-		assertEquals(newHegaldiBidaiarenIraupena,hegaldiZerbitzu.getHegaldiBidaiarenIraupena());
-	
-	}
-
-	
-
-	@Test
-	public void testGetSetHegaldiPrezioa() {
-		hegaldiZerbitzu.setHegaldiPrezioa(22);
-		assertEquals(22,hegaldiZerbitzu.getHegaldiPrezioa(),0.01);
-	}
-
-	
-
-	@Test
-	public void testGetSetHegaldiJatorrizkoAireportua() {
-		hegaldiZerbitzu.setHegaldiJatorrizkoAireportua("Malaga");
-		assertEquals("Malaga",hegaldiZerbitzu.getHegaldiJatorrizkoAireportua());
-	}
-
-
-
-	@Test
-	public void testGetSetHegaldiHelmugakoAireportua() {
-		hegaldiZerbitzu.setHegaldiHelmugakoAireportua("Malaga");
-		assertEquals("Malaga",hegaldiZerbitzu.getHegaldiHelmugakoAireportua());
-
-	}
-
-	
-
-	@Test
-	public void testGetSetAirelinaKodea() {
-		hegaldiZerbitzu.setAirelinaKodea("A2");
-		assertEquals("A2",hegaldiZerbitzu.getAirelinaKodea());
-	}
-
-	
-
-	@Test
-	public void testGetSetHegaldiKodeaEtorri() {
-		hegaldiZerbitzu.setHegaldiKodeaEtorri("E2");
-		assertEquals("E2",hegaldiZerbitzu.getHegaldiKodeaEtorri());
-
-	}
-
-	
-
-	@Test
-	public void testGetSetItzuleraOrdua() {
-		Time newItzuleraOrdua = Time.valueOf("03:00:00");
-		hegaldiZerbitzu.setItzuleraOrdua(newItzuleraOrdua);
-		assertEquals(newItzuleraOrdua,hegaldiZerbitzu.getItzuleraOrdua());
-	
-	}
-
-	
-
-	@Test
-	public void testGetSetEtorriaEguna() {
-		Date newDate = Date.valueOf("2025-02-17");
-		hegaldiZerbitzu.setEtorriaEguna(newDate);
-		assertEquals(newDate, hegaldiZerbitzu.getEtorriaEguna());
-
-	}
-
-	
-
-	@Test
-	public void testGetSetBueltakoIraupena() {
-		Time newBueltakoIraupena = Time.valueOf("03:00:00");
-		hegaldiZerbitzu.setBueltakoIraupena(newBueltakoIraupena);
-		assertEquals(newBueltakoIraupena,hegaldiZerbitzu.getBueltakoIraupena());
-	
-	}
-
-	
-
-	@Test
-	public void testGetSetJoanJatorrizkoAireportua() {
-		hegaldiZerbitzu.setJoanJatorrizkoAireportua("Santander");
-		assertEquals("Santander",hegaldiZerbitzu.getJoanJatorrizkoAireportua());
-	}
-
-	
-
-	@Test
-	public void testGetSetJoanHelmugakoAireportua() {
-		hegaldiZerbitzu.setJoanHelmugakoAireportua("Santander");
-		assertEquals("Santander",hegaldiZerbitzu.getJoanHelmugakoAireportua());
-	
-	}
-
-	
-
-	@Test
-	public void testGetSetBueltakoAirelineaKodea() {
-		hegaldiZerbitzu.setBueltakoAirelineaKodea("B2");
-		assertEquals("B2",hegaldiZerbitzu.getBueltakoAirelineaKodea());
-	}
-	
-	@Test
+    // Zerbitzuaren toString metodoa testatzen duen metodoa
+    @Test
     public void testToStringTxt() {
-        String expected ="\n" +
+        String expected = "\n" +
                 "Zerbitzuaren izena = Ostatu Zerbitzua\n" +
                 "Ostatu Prezioa = 100.0\n" +
                 "Ostatu sarrera eguna = 2025-02-13\n" +
@@ -306,6 +264,7 @@ public class ZerbitzuTest {
                 "Joan helmugako aireportua = null\n" +
                 "Bueltako airelinea = null\n";
                 
-        assertEquals(expected, ostatuZerbitzu.toStringTxt() );
+        assertEquals(expected, ostatuZerbitzu.toStringTxt());
     }
 }
+
